f48ddd65b670cbdd0efa808147f78aec451b6991
Luawig <2453576227@qq.com>
2019-04-14 22:04:28 +0800

    initialization

---
+++ temp.js
@@ -0,0 +1,31 @@
+function Function1 () {
+    window.location.href = "https://putian.xueanquan.com/";
+}
+
+function Function2 () {
+    window.location.href = "https://ciwong.com/index.html";
+}
+
+function Function3 () {
+    window.location.href = "https://mail.qq.com/";
+}
+
+function Function4 () {
+    window.location.href = "https://www.weisheng.cf/games/RandQAQ/";
+}
+
+function Function5 () {
+    window.location.href = "https://www.bilibili.com/";
+}
+
+function Function6 () {
+    window.location.href = " ";
+}
+
+function Function7 () {
+    window.location.href = " ";
+}
+
+function Function8 () {
+    window.location.href = " ";
+}
