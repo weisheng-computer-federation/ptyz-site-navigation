9fc8f1aeba58a0a7e3159a53cf9ac900ddff18c9
beautyyuyanli <beautyyuyanli@outlook.com>
2019-05-12 16:53:43 +0800

    add many things

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var flag = 1;
 function Search () {
     var input = document.getElementById("search_text").value;
-    if (flag == 1)
+    if ($('.fir')[0].id == 'bing')
         window.open("https://cn.bing.com/search?q=" + input, "blank");
     else
         window.open("https://www.baidu.com/s?ie={inputEncoding}&wd=" + input, "blank");
@@ -13,15 +13,6 @@ function Check () {
 }
 
 function Change (x) {
-    if (x == 1 && flag == 2) {
-        $("#search_fir").css("padding", "0 15px");
-        $("#search_sec").css("padding", "0");
-        flag = 1;
-    }
-    else if (x == 2 && flag == 1) { 
-
-        $("#search_fir").css("padding", "0");
-        $("#search_sec").css("padding", "0 15px");
-        flag = 2;
-    }
+    $('.search_engine').attr('class','search_engine');
+    $(x).attr('class','search_engine fir');
 }
